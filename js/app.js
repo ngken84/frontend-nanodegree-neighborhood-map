9a2b4b09984be090165dfe3c767b111323c3339a
Ken <ng.ken84@gmail.com>
2015-08-18 23:05:16 -0700

    Update knockout functionality

---
+++ js/app.js
@@ -0,0 +1,50 @@
+
+function initialize() {
+
+	ko.applyBindings(new MapViewModel());
+	// var myLocation = new google.maps.LatLng(37.4434263, -122.176138);
+
+	// map = new google.maps.Map(document.getElementById('map'), {
+	// 	center: myLocation,
+	// 	zoom: 17
+	// });
+
+	// var request = {
+	// 	location : myLocation,
+	// 	radius : '500',
+	// 	types: ['store']
+	// }
+
+	// function callback(results, status) {
+	// 	if(status == google.maps.places.PlacesServiceStatus.OK) {
+	// 		for (var i = 0, x = results.length; i < x; i++) {
+	// 			console.log(i + " : " + results[i].name);
+	// 		}
+	// 	}
+	// }
+
+	// placesService = new google.maps.places.PlacesService(map);
+	// placesService.nearbySearch(request, callback);
+}
+
+var MapViewModel = function() {
+	var self = this;
+
+	self.longitude = ko.observable(37.4434263);
+	self.latitude = ko.observable(-122.176138);
+
+	self.myLocation = ko.computed(function() {
+		return new google.maps.LatLng(self.longitude(), self.latitude());
+	});
+
+	self.map = new google.maps.Map(document.getElementById('map'), {
+		center: self.myLocation(),
+		zoom: 17
+	});
+
+
+
+
+
+
+}
