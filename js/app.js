614fc05a62a6d84ba0e7db7884ff7b6994f906e1
Ken <ng.ken84@gmail.com>
2015-08-30 20:58:43 -0700

    Add comments

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-
+/*
+Initialize runs when the after the google map is finished loading. Sets up the ViewModel and side panel
+*/
 function initialize() {
 	ko.applyBindings(new MapViewModel());
 
@@ -17,12 +19,17 @@ function initialize() {
 	});
 };
 
+/*
+Contains the options for the Google API search.
+*/
 var MapOptions = function() {
 	var self = this;
 
+	//Latitude and Longitude of place of interest. Set for 69 University Dr. Menlo Park CA.
 	self.latitude = 37.4434263;
 	self.longitude = -122.176138;
 
+	// Sets the types of places the user will be searching for.
 	self.placeTypePrefs = [
 		new PlaceType('Airport', 'airport', false),
 		new PlaceType('Amusement Park', 'amusement_park', false),
@@ -51,8 +58,11 @@ var MapOptions = function() {
 		new PlaceType('Store', 'store', true),
 		new PlaceType('Zoo', 'zoo', false)
 	];
+
+	// Sets the range of the search
 	self.range = ko.observable('500');
 
+	// Sets up the Map Options objects. Looks in localStorage for existing preferences, but if they do not exist use default ones.
 	self.initialize = function() {
 		var localSettings = localStorage.getItem('kenmap-mapOptions');
 		if(localSettings) {
@@ -71,10 +81,12 @@ var MapOptions = function() {
 
 	self.initialize();
 
+	// gets Google LatLng object.
 	self.getLocation = function() {
 		return new google.maps.LatLng(self.latitude, self.longitude);
 	};
 
+	//Returns google options object.
 	self.getOptionsObject = function () {
 		var placeTypeArray = [];
 		for(var i = 0, x = self.placeTypePrefs.length; i < x; i++) {
@@ -89,41 +101,49 @@ var MapOptions = function() {
 		}
 	};
 
+	//Update location of center of the map.
 	self.setLocation = function(lat, lon) {
 		self.latitude = lat;
 		self.longitude = lon;
 	};
 
+	//Updates local storage with map preferences
 	self.updateLocalStorage = function() {
 		var stringOptions = JSON.stringify(ko.toJS(self));
 		localStorage.setItem('kenmap-mapOptions', stringOptions);
 	}
 };
 
+/*
+Place type option.
+*/
 var PlaceType = function(name, type, selected) {
 	var self = this;
 	self.typeName = type;
+	self.placeTypeId = "map_options_" + type;
 	self.name = name;
 	self.isSelected = ko.observable(selected);
 };
 
+/*
+Map Place object contains an place returned by the google places api. Also contains information pulled from various services on those places.
+*/
 var MapPlace = function(googlePlace, googleMap, iconLabel, openModalFunction) {
 	var self = this;
 
 	self.placeResult = googlePlace;
-	console.log(googlePlace);
-	if(self.placeResult.photos) {
-		for(var i = 0, x = self.placeResult.photos.length; i < x; i++) {
-			console.log(self.placeResult.photos[i].getUrl({maxWidth: 270}));
-		}
-	}
+
+	//Image URL base on google street view api
 	self.imageUrl = "http://maps.googleapis.com/maps/api/streetview?size=560x200&location=" + self.placeResult.geometry.location.G + ","+ self.placeResult.geometry.location.K;
 	self.labelIcon = iconLabel;
 
+
+	// Data from wikipedia
 	self.isWikiLoaded = ko.observable(false);
 	self.isWikiFailed = ko.observable(false);
 	self.wikiInformation = ko.observableArray([]);
 
+	// Loads data from Wikipedia web service.
 	self.loadWikipediaData = function() {
 		if(!self.isWikiLoaded()) {
 			$.ajax('https://en.wikipedia.org/w/api.php?action=opensearch&search=' + self.placeResult.name + '&prop=revisions&rvprop=content&format=json&callback=wikiCallback',
@@ -131,7 +151,7 @@ var MapPlace = function(googlePlace, googleMap, iconLabel, openModalFunction) {
 					dataType: 'jsonp',
 					jsonp: 'callback',
 					success: function(data, textStatus, jq) {
-						if(data && data.length === 4 && data[1].length > 0) {
+						if(textStatus === "success" && data && data.length === 4 && data[1].length > 0) {
 							var array = [];
 							for(var i = 0, x = Math.min(data[1].length, 5); i < x; i++)
 							{
@@ -157,26 +177,32 @@ var MapPlace = function(googlePlace, googleMap, iconLabel, openModalFunction) {
 		}
 	};
 
+	// Data from New Yorks Times web service
 	self.isNYTimesLoaded = ko.observable(false);
 	self.isNYTimesFailed = ko.observable(false);
 	self.nytInformation = ko.observableArray([]);
 
+	// Loads data from the New York Times web service
 	self.loadNYTimesData = function() {
 		if(!self.isNYTimesLoaded()) {
 			var NYT_URL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + self.placeResult.name + '&api-key=6cbb4b8bb025f0865f9afc276c97f269:8:72368218';
-			$.getJSON(NYT_URL, function(data) {
-				var articleArray = data.response.docs;
-				var array = [];
-				for(var i = 0, x = Math.min(articleArray.length,5); i < x; i++) {
-					var article = articleArray[i];
-					array.push({
-						name: article.headline.main,
-						link: article.web_url,
-						desc: article.snippet
-					});
-				}
-				self.nytInformation(array);
-				if(array.length === 0) {
+			$.getJSON(NYT_URL, function(data, textStatus) {
+				if(textStatus ==="success" && data && data.response.docs) {
+					var articleArray = data.response.docs;
+					var array = [];
+					for(var i = 0, x = Math.min(articleArray.length,5); i < x; i++) {
+						var article = articleArray[i];
+						array.push({
+							name: article.headline.main,
+							link: article.web_url,
+							desc: article.snippet
+						});
+					}
+					self.nytInformation(array);
+					if(array.length === 0) {
+						self.isNYTimesFailed(true);
+					}
+				} else {
 					self.isNYTimesFailed(true);
 				}
 			}).fail(function() {
@@ -187,12 +213,15 @@ var MapPlace = function(googlePlace, googleMap, iconLabel, openModalFunction) {
 		}
 	}
 
+	//Data from Google Place Details web service
 
+	// gets google maps LatLng object for teh place
 	self.getLocation = function() {
 		var location = self.placeResult.geometry.location;
 		return new google.maps.LatLng(location.G, location.K);
 	};
 
+	// creates a marker on the map
 	self.createMarker = function(gMap, clickFunction) {
 		self.marker = new google.maps.Marker({
 			position : self.getLocation(),
@@ -200,6 +229,7 @@ var MapPlace = function(googlePlace, googleMap, iconLabel, openModalFunction) {
 			label : iconLabel
 		});
 
+		// Adds click listener for the map marker.
 		self.marker.addListener('click', function() {
 			if(clickFunction) {
 				clickFunction(self);
@@ -207,6 +237,7 @@ var MapPlace = function(googlePlace, googleMap, iconLabel, openModalFunction) {
 		});
 	}
 
+	// removes the marker from the map
 	self.removeMarker = function() {
 		if(self.marker) {
 			self.marker.setMap(null);
@@ -216,6 +247,7 @@ var MapPlace = function(googlePlace, googleMap, iconLabel, openModalFunction) {
 
 	self.createMarker(googleMap, openModalFunction);
 
+	// returns true if the text passed is contained in the map place's name or address
 	self.doesMatchFilter = function(filterText) {
 		if(filterText) {
 			if(filterText.length == 0) {
@@ -230,21 +262,27 @@ var MapPlace = function(googlePlace, googleMap, iconLabel, openModalFunction) {
 	self.userData = new UserData(self.placeResult['place_id']);
 };
 
+/*
+User data for a map place. Contains user comments and rating.
+*/
 var UserData = function(googleCode) {
 	var self = this;
-
+	// google place code
 	self.googleCode = googleCode;
 
+	// user rating of the place
 	self.rating = ko.observable(0);
 
+	// list of user comments. Object contains a comment and the date the comment was made.
 	self.commentArray = ko.observableArray([]);
-
 	self.newComment = ko.observable("");
 
+	// generates the key used for the localStorage data.
 	self.getLocalStorageKey = function() {
 		return 'kenmap-' + self.googleCode;
 	};
 
+	// initializes the User Data object. If data exists locally then grab it.
 	self.initialize = function() {
 		var key = self.getLocalStorageKey();
 		var localData = localStorage.getItem(key);
@@ -255,8 +293,7 @@ var UserData = function(googleCode) {
 		}
 	};
 
-
-
+	// adds a comment if the newComment object is not empty. Also updates the local storage object.
 	self.addComment = function() {
 		if(self.newComment().length > 0) {
 			var d = new Date();
@@ -265,7 +302,8 @@ var UserData = function(googleCode) {
 				date: dateStr,
 				comment: self.newComment()
 			});
-			console.log(self.googleCode);
+
+			// if the objects has a googleCode, then update the local storage object.
 			if(self.googleCode) {
 				localStorage.setItem(self.getLocalStorageKey(), JSON.stringify(ko.toJS(self)));
 			}
@@ -276,26 +314,46 @@ var UserData = function(googleCode) {
 	self.initialize();
 };
 
+
+/*
+The actual map view model.
+*/
 var MapViewModel = function() {
 	var self = this;
+
+	// This contains all the places return by the places API
 	self.placesArray = [];
+
+	// This contains the list of places that will be shown on the map.
 	self.placesFilteredArray = ko.observableArray([]);
+
+	// The current place displayed in the modal
 	self.currentPlace = ko.observable(null);
+
+	// The search phrase that will filter the shown places
 	self.searchTerm = ko.observable('');
 
+	// Map options object that dictates what places are displayed on the map
 	self.mapOptions = new MapOptions();
-	self.isLoading = ko.observable(true);
 
+	// Dictates several states
+	self.isLoading = ko.observable(true);
+	self.isEmpty = ko.observable(false);
+	self.isError = ko.observable(false);
+
+	// The Google Map Object
 	self.map = new google.maps.Map(document.getElementById('map'), {
 		center: self.mapOptions.getLocation(),
 		zoom: 17
 	});
 
+	// Places Service Object
 	self.placesService = new google.maps.places.PlacesService(self.map);
+
 
 	self.placeServiceCallback = function (results, status) {
 		if(status == google.maps.places.PlacesServiceStatus.OK) {
-			var labels = 'ABCDEFGHIJKLMNOP';
+			var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 			self.resetMarkers();
 			self.placesArray = [];
 			self.placesFilteredArray.removeAll();
@@ -303,21 +361,36 @@ var MapViewModel = function() {
 				self.placesArray.push(new MapPlace(results[i], self.map, labels[i], self.openModal));
 			}
 			self.placesFilteredArray(self.placesArray);
+			self.isEmpty(false);
+			self.isError(true);
 			self.isLoading(false);
+			console.log("Hello");
+		} else if(status == google.maps.places.PlacesServiceStatus.ZERO_RESULTS) {
+			self.isLoading(false);
+			self.isError(false);
+			self.isEmpty(true);
+		} else {
+			self.isLoading(false);
+			self.isError(true);
+			self.isEmpty(false);
 		}
 	}
 
+	// Removes all markers on the map
 	self.resetMarkers = function() {
 		for(var i = 0, x = self.placesArray.length; i < x; i++) {
 			self.placesArray[i].removeMarker();
 		}
 	}
 
+	// Opens the modal
 	self.openModal = function(mapPlace) {
 		self.selectPlace(mapPlace);
 		$('#myModal').modal('show');
 	}
 
+
+	// Does a new search base on the current MapOptions object.
 	self.updatePlaces = function() {
 		self.isLoading(true);
 		self.resetMarkers();
@@ -326,12 +399,14 @@ var MapViewModel = function() {
 		self.mapOptions.updateLocalStorage();
 	}
 
+	// Updates the current place
 	self.selectPlace = function(mapPlace) {
 		self.currentPlace(mapPlace);
 		mapPlace.loadWikipediaData();
 		mapPlace.loadNYTimesData();
 	};
 
+	// Filters the current places array based on the search term
 	self.filterPlaces = function() {
 		var filter = self.searchTerm();
 		var newArray = [];
@@ -346,12 +421,15 @@ var MapViewModel = function() {
 	};
 	self.updatePlaces();
 
-
 	self.geocoder = new google.maps.Geocoder();
 
+	// Entry for a new address in the Search bar
 	self.newAddress = ko.observable("");
+
+	// If the new address has multiple possible results it is contained here.
 	self.partialAddressList = ko.observableArray([]);
 
+	// Searches for a new neighborhood
 	self.updateAddress = function() {
 		var address = self.newAddress();
 		if(address && address.length > 0) {
@@ -381,6 +459,7 @@ var MapViewModel = function() {
 		}
 	};
 
+	// Call back for when a user selects a new neighborhood location in a list.
 	self.selectNewAddress = function(place) {
 		self.mapOptions.setLocation(place.geometry.location.G, place.geometry.location.K);
 		self.map.setCenter(place.geometry.location);
